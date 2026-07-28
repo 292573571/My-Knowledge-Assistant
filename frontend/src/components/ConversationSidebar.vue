c6fbf36e62f1c821c5c85600c28d46cd0a996fd5
95h <292573571@qq.com>
2026-07-28 19:18:57 +0800

    接口请求方式转换为SSE流式请求

--- frontend/src/components/ConversationSidebar.vue
+++ frontend/src/components/ConversationSidebar.vue
@@ -43,8 +43,8 @@ function formatRelativeTime(value) {
             <strong>{{ conversation.title }}</strong>
             <small>{{ formatRelativeTime(conversation.updatedAt) }}</small>
           </button>
-        <button type="button" class="conversation-delete" aria-label="删除会话" :disabled="deletingDisabled" @click.stop="$emit('delete', conversation.id)">
-          ×
+        <button type="button" class="conversation-delete" title="删除会话" aria-label="删除会话" :disabled="deletingDisabled" @click.stop="$emit('delete', conversation.id)">
+          <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M4 7h16M9 7V5h6v2m-8 0 1 13h8l1-13M10 11v5m4-5v5" /></svg>
         </button>
       </div>
     </nav>
